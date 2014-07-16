8696a63ffdf9b9e69f4276d2c4433ead6c12158d
Christophe Marchal <toff63@gmail.com>
2014-07-16 17:42:08 -0300

    added cenario produto model

---
+++ src/calculo_tributos/cenario_produto.clj
@@ -0,0 +1,36 @@
+(ns calculo-tributos.cenario_produto)
+
+(defstruct IcmsSt :aliquota :valorPauta :redBC :dispositivoLegal :bcComposicao :cargaMedia :mva :mvaAjustado :valor)
+(defstruct Icms :aliquota :valorPauta :dispositivoLegal :redBC :credito :bcComposicao :valor :creditoDispositivoLegal :indicadorCredito :cst)
+(defstruct Ipi  :aliquota 
+	            :credito 
+	            :dispositivoLegal 
+	            :cst 
+	            :pRedBC 
+	            :pRedAliq 
+	            :pRedValImp 
+	            :valor 
+	            :creditoDispositivoLegal 
+	            :indicadorCredito)
+(defstruct Pis  :aliquota 
+				:aliquotaEspecifica
+	            :credito 
+	            :dispositivoLegal 
+	            :cst 
+	            :pRedBC 
+	            :valor 
+	            :creditoDispositivoLegal 
+	            :indicadorCredito)
+
+(defstruct Cofins  	:aliquota 
+				    :aliquotaEspecifica
+	               	:credito 
+	            	:dispositivoLegal 
+	            	:cst 
+	            	:pRedBC 
+	            	:valor 
+	            	:creditoDispositivoLegal 
+	            	:indicadorCredito)
+
+(defstruct Anticipacao :tipo :valorPauta :pRedBCInternaDest :aliqInternaDest :responsavel :mvaAjustado :percFixo :valor)
+(defstruct CenarioProduto :idCenario :idProduto :icms :icmsSt :ipi :pis :cofins :antecipacao :ufOrigem :ufDestino :valor)
