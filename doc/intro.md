672ad6564eff75bec68ff27df830c788869c0790
Joel Correa <joel.correa@ilegra.com>
2014-07-16 17:06:02 -0300

    Created initial project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to calculo-tributos
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/calculo_tributos/core_test.clj
@@ -0,0 +1,7 @@
+(ns calculo-tributos.core-test
+  (:require [clojure.test :refer :all]
+            [calculo-tributos.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
